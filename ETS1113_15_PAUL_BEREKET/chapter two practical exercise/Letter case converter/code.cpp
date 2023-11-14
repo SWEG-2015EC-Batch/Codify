fb6733e7019ccb1f1b4d2c221eef04ab9c68d702
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-11-14 13:31:53 -0800

    Update code.cpp

--- ETS1113_15_PAUL_BEREKET/chapter two practical exercise/Letter case converter/code.cpp
+++ ETS1113_15_PAUL_BEREKET/chapter two practical exercise/Letter case converter/code.cpp
@@ -1,1 +1,26 @@
+//this code accepts a letter and converts to uppercase if small and viseversa.
 
+#include <iostream>
+
+#include <ctype.h>
+
+using namespace std;
+
+int main(){
+
+    char letter;
+
+    cout << "please enter a letter: ";
+    cin >> letter;
+
+    if (isupper(letter)){
+        letter = tolower(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else if (islower(letter)){
+        letter = toupper(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else {
+        cout << "Invalid input";
+    }
