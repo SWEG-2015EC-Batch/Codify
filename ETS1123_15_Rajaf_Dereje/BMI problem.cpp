9e6ecb31eb00907f20d046305d2b85f017474920
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-11-14 23:54:07 +0300

    Update BMI problem.cpp

--- ETS1123_15_Rajaf_Dereje/BMI problem.cpp
+++ ETS1123_15_Rajaf_Dereje/BMI problem.cpp
@@ -1,1 +1,19 @@
+//this program finds body mass index
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout<<"enter your mass in kilogram";
+   double M;
+   cin>>M;
+   cout<<"enter your height in meter";
+   double H;
+   cin>>H;
+   double BMI;
+   BMI=M /(H*H);
+   cout<<"your BMI is :"<<BMI;
+    return 0;
+}
 
