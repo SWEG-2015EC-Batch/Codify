41d07b02b683cbc479ef945bf02c7c7a39848f93
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-11-15 00:25:13 +0300

    Update data transmission Time problem.cpp

--- ETS1123_15_Rajaf_Dereje/Time problem.cpp
+++ ETS1123_15_Rajaf_Dereje/Time problem.cpp
@@ -1,1 +1,20 @@
+//this program finds the time it takes to transfer 
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int data;
+    double second, minuit, hour, day;
+    cout<<"enter number of bytes of your data :";
+    cin>>data;
+    second = data/960;
+    minuit = second /60;
+    hour = minuit/60;
+
+    day =hour/24;
+    cout<<"the transmission of this file will take : "<<second<< "sec = " <<minuit<< "min = "<< hour<<"hr = "<<day<<"dy ";
+
+    return 0;
+}
