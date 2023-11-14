edf9e3904534575d63a01105b789ae44f0a170c1
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-11-15 00:43:11 +0300

    Update power problem.cpp

--- ETS1123_15_Rajaf_Dereje/power problem.cpp
+++ ETS1123_15_Rajaf_Dereje/power problem.cpp
@@ -1,1 +1,18 @@
+//this program gives us the exponential function value of two numbers 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ double x,y,z;
+ cout<<"enter the base number :";
+ cin>>x;
+ cout<<"enter the exponent :";
+ cin>>y;
+ z = pow(x,y) ;
+ cout<<"the resulting value is :"<<z;
+    return 0;
+}
 
