ca365a21666e1c3bf97837090bb5af8f103c8614
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-11-14 13:58:02 -0800

    Update code.cpp

--- ETS1113_15_PAUL_BEREKET/chapter two practical exercise/power calculator/code.cpp
+++ ETS1113_15_PAUL_BEREKET/chapter two practical exercise/power calculator/code.cpp
@@ -1,1 +1,21 @@
+//this program solves power problems by taking the base number and its exponent.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter the base number:" ;
+    int x ;
+    cin>> x ;
+    cout<< "Enter the number on the exponent:" ;
+    int y ;
+    cin>> y;
+    int Z ;
+    Z = pow(x,y);
+    cout << x <<  "the power of" << y << "is " << Z ;
+    return 0;
+}
 
