867aee55f476844f5892ba4ee7ef326e7f831afe
Mikiyas Mechalo <77005039+mikiyasmechalo@users.noreply.github.com>
2023-11-14 17:19:08 +0300

    Create code.cpp
    Exercise 1 q 2

---
+++ ETS0944_15_Mikiyas_Mechalo/2nd year/Exercise 1/3. letter problem/code.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+int main(){
+
+    char letter;
+
+    cout << "please enter a letter: ";
+    cin >> letter;
+
+    if (isupper(letter)){
+        letter = tolower(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else if (islower(letter)){
+        letter = toupper(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else {
+        cout << "Invalid input";
+    }
+    
+
+   
+
+
+
+
+    return 0;
+} 
