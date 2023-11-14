b269a9d441c8ce18b09bc3aa7e516f1d13e45733
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-11-14 13:27:50 -0800

    Update code.cpp

--- ETS1113_15_PAUL_BEREKET/chapter two practical exercise/Fuel economy/code.cpp
+++ ETS1113_15_PAUL_BEREKET/chapter two practical exercise/Fuel economy/code.cpp
@@ -1,1 +1,16 @@
+// This code calculates  disatnce a car can tarvel without refueleing by taking tank's capacity and miles per gallon as inputs
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    float capacity, miles_per_galon, fuel_economy;
+    cout <<"what is the fuel tank's capacity in gallon?: " ;
+    cin>> capacity;
+    cout<<" how many miles can this car travel per gallon?: " ;
+    cin>> miles_per_galon ;
+    fuel_economy = capacity*miles_per_galon ;
+    cout<< " you can travel " << fuel_economy << "miles without refuling." << endl;
+    return 0;
+}
