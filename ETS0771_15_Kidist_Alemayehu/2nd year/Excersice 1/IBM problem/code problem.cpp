bc199bb3af25c6d81379072764789e6a122eb29b
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-15 00:54:43 -0800

    Update code problem.cpp

--- ETS0771_15_Kidist_Alemayehu/2nd year/Excersice 1/IBM problem/code problem.cpp
+++ ETS0771_15_Kidist_Alemayehu/2nd year/Excersice 1/IBM problem/code problem.cpp
@@ -1,1 +1,21 @@
+// this programe calculate BMI by taking height and weight
 
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    double weight, height, BMI;   // BMI means body mass index
+    
+    cout<<" Enter your weigt in kg: ";
+    cin>> weight;
+    cout<<" Enter your height in meter: ";
+    cin>> height;
+
+    cout<<"Your BMI is: "<< weight/pow(height, 2);
+    
+    return 0;
+
+}
