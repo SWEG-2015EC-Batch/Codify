23c9a1a706c970cb6f7bbd79d517249220499c31
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-15 02:38:06 -0800

    Update power.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question4/power.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question4/power.cpp
@@ -1,1 +1,22 @@
+// this programe will give output in a power fromat
 
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+
+int main()
+{
+    
+    double x,y;
+    cout<<"enter x from the keyboard: ";
+    cin>>x;
+    cout<<" Enter y from the keyboard: ";
+    cin>>y;
+
+    cout<<"This is x the power of y result: "<<pow(x, y);
+
+    return 0;
+
+}
