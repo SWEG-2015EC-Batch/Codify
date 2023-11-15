5090a1b94b748f493f463a2342b50a8e67f1deb8
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-11-15 14:00:04 +0300

    Update code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q1 BMI Problem/code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q1 BMI Problem/code.cpp
@@ -1,1 +1,13 @@
+//This program calculates the distance that  can be traveled without refueling for an automobile given the size of the fuel tank and miles per gallon
+#include <iostream>
+int main() {
+    using namespace std;
+    float h, w, BMI; // h = height, w = weight and BMI = body mass index
+    cout<<" enter your weight in kilograms:- ";
+    cin>>w;
+     cout<<" enter your height in meters:- ";
+    cin>>h;
+    cout<<" BMI:- "<< w/(h*h);
 
+    return 0;
+}
