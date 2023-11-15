8ee09f41007adf22ab6c1e18fa07ee2a7b7f5017
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-15 02:21:46 -0800

    Update fuel.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question2/fuel.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question2/fuel.cpp
@@ -1,1 +1,16 @@
+// this programe calculate the distance an autombile can go with out refueling
+#include<iostream>
 
+using namespace std;
+int main(){
+    double capacity, psd, Mg;
+    cout<<" Enter the automible fuel tank capacity: ";
+    cin>> capacity;
+    cout<<" Enter the mile per gallon for for automobile: ";
+    cin>> Mg;
+
+    psd = capacity * Mg;
+    cout<<"the automible can go a distance of "<< psd<< " mile ith out refueling";
+
+    return 0;
+}
