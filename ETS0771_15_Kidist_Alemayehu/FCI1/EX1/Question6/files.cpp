b1cc143524a34146ec004d9fc30e85fa54889bef
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-15 02:44:14 -0800

    Update files.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question6/files.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question6/files.cpp
@@ -1,1 +1,23 @@
+// questin 6 is about calculating how long it takes to calculate 
 
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int second, minute, hour, day, filesize;
+    cout<< " enter the file size in byte: ";
+    cin>> filesize;
+
+    second = filesize/960;
+    minute = second/60;
+    hour = minute/60;
+    day = hour/24;
+
+    cout<<" Time in second is: "<< second;
+    cout<<" Time in minute is: "<< minute;
+    cout<<" Time in hour is: "<< hour;
+
+    return 0;
+}
