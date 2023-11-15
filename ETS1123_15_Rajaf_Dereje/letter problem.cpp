b3e6882e10aed9a15405b1996a46cb3b2e7a2ff9
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-11-15 07:27:00 +0300

    Update letter problem.cpp

--- ETS1123_15_Rajaf_Dereje/letter problem.cpp
+++ ETS1123_15_Rajaf_Dereje/letter problem.cpp
@@ -1,1 +1,29 @@
+//this program converts a given letter into its upper or lower class counter fit
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+int main(){
+
+    char letter;
+
+    cout << "please enter a letter: ";
+    cin >> letter;
+
+    if (isupper(letter)){
+        letter = tolower(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else if (islower(letter)){
+        letter = toupper(letter);
+        cout << "Your letter in upper case is: " << letter ;
+    }
+    else {
+        cout << "Invalid input";
+    }
+
+
+   return 0;
+}
 
