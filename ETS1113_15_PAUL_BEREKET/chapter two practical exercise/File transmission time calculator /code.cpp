e350fb6825254c3e9e5636569c881c6c6bf22a9c
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-11-14 21:12:29 -0800

    Update code.cpp

--- ETS1113_15_PAUL_BEREKET/chapter two practical exercise/File transmission time calculator /code.cpp
+++ ETS1113_15_PAUL_BEREKET/chapter two practical exercise/File transmission time calculator /code.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int transmissionRate = 960; // characters per second
+    cout<<" enter the file size in byte:" ;
+    int sizeInByte ;  // file size in bytes
+    cin >> sizeInByte ;
+
+    int timeInSeconds = sizeInByte / transmissionRate;
+    int hours = timeInSeconds / 3600;
+    int minutes = (timeInSeconds % 3600) / 60;
+    int seconds = timeInSeconds % 60;
+
+    cout << "It will take " << hours << " hours, " << minutes << " minutes, and " << seconds << " seconds to send the file." << endl;
+
+    return 0;
+}
