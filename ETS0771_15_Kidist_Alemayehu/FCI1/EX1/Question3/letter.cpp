cfe5aa11886bd322f1b803d3eefe622f90d81cd9
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-15 02:31:08 -0800

    Update letter.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question3/letter.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX1/Question3/letter.cpp
@@ -1,1 +1,28 @@
+// This programe will display the letter respctive uppercae or lowercase letter
 
+#include<iostream>
+using namespace std;
+#include<ctype.h>
+
+
+int main()
+{
+    char x;
+    cout<<"enter x: ";
+    cin>>x;
+
+    if (islower(x))
+    {
+        cout<<"uppercase: " <<(char)toupper(x)<<endl;
+    }
+    else if (isupper(x))
+    {
+        cout<<"lowercase: "<<(char)tolower(x)<<endl;
+    }
+    else
+    {
+        cout<<"invalid input"<<endl;
+    }
+
+    return 0;
+}
