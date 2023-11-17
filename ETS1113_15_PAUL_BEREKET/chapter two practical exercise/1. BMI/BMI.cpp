f3ffd128e5cd49c4d02cb340f4b7e34578761926
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-11-17 03:51:57 -0800

    Update BMI.cpp

--- ETS1113_15_PAUL_BEREKET/chapter two practical exercise/1. BMI/BMI.cpp
+++ ETS1113_15_PAUL_BEREKET/chapter two practical exercise/1. BMI/BMI.cpp
@@ -1,21 +1,22 @@
 //this code calculates the Body mass index by accepting mass and height
 #include <iostream>
 
+#include <cmath>
 using namespace std;
 
 int main(){
 
     float height, mass, BMI;
-    cout << "Please enter your mass in kilograms:" ;
+    cout << "Please enter your mass in kilograms: " ;
     cin >> mass;
-    cout << "Please enter your height in meters:" ;
+    cout << "Please enter your height in meters: " ;
     cin >> height;
-    BMI = mass/ (height * height);
-
-    cout << "Your Body Mass Index is: " << BMI<<"gram per meter squared" << endl ;
+    BMI= mass/pow(height,2);
+    cout << "Your Body Mass Index is: " << mass/pow(height,2) << endl ;
 
     //This code is optional and was added to improve the response
-    if (BMI <= 18.5){
+   BMI= mass/pow(height,2);
+        if (BMI <= 18.5){
         cout << "You are underweight, You should start taking more calories than your expenditure.";
     }
     else if (BMI > 18.5 && BMI <= 25){
@@ -26,4 +27,5 @@ int main(){
 
     return 0;
     }
+
 
