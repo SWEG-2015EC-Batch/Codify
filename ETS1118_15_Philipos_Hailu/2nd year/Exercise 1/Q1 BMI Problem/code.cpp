0be78514d802de1a0f34961bef58eb7d113d380c
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-11-17 09:54:21 +0300

    Update code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q1 BMI Problem/code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q1 BMI Problem/code.cpp
@@ -1,8 +1,8 @@
-//This program calculates the distance that  can be traveled without refueling for an automobile given the size of the fuel tank and miles per gallon
+// A program that calculates and display the Body Mass Index (BMI) of a person. 
 #include <iostream>
 int main() {
     using namespace std;
-    float h, w, BMI; // h = height, w = weight and BMI = body mass index
+    float h, w; // h = height and w = weight 
     cout<<" enter your weight in kilograms:- ";
     cin>>w;
      cout<<" enter your height in meters:- ";
