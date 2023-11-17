b53127149a097720fdbf1c1970a7e3f3224f74a9
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-11-17 21:19:35 +0300

    Update code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q4  Power problem/code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q4  Power problem/code.cpp
@@ -1,1 +1,14 @@
+// 
+#include <iostream>
+#include <math.h>
+int main() {
+    using namespace std;
+    float X, Y;// X = base number and Y = exponent number
+cout<<" enter the base number:- ";
+cin>>X;
+cout<<" enter the exponent number:- ";
+cin>>Y;
+cout<<" the result is:- "<< pow(X,Y);
 
+    return 0;
+}
