24ac1a9364c8ac36b9e07324b55a18f32b1e37cc
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-11-17 23:19:18 +0300

    Update power problem.cpp

--- ETS1123_15_Rajaf_Dereje/power problem.cpp
+++ ETS1123_15_Rajaf_Dereje/power problem.cpp
@@ -1,4 +1,4 @@
-//this program gives us the exponential function value of two numbers 
+//this program finds takes base number and exponet number and fides their power
 #include <iostream>
 #include <cmath>
 
@@ -6,13 +6,12 @@ using namespace std;
 
 int main()
 {
- double x,y,z;
+ double x,y;
  cout<<"enter the base number :";
  cin>>x;
  cout<<"enter the exponent :";
  cin>>y;
- z = pow(x,y) ;
- cout<<"the resulting value is :"<<z;
+ pow(x,y) ;
+ cout<<"the resulting value is :"<<pow(x,y);
     return 0;
 }
-
