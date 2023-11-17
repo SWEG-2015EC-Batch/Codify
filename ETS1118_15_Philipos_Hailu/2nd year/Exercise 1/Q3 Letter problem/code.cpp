4e13bfedaf4f87a9b29ebe82c6cc2119083cfcc6
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-11-17 20:28:00 +0300

    Update code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q3 Letter problem/code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q3 Letter problem/code.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+int main() {
+    char letter;
+    cout<< "enter a letter:- ";
+    cin>> letter;
+    if (islower(letter)){
+        letter = toupper(letter);
+        cout<<" your letter in upper case:- "<< letter;
+    }
+   else if (isupper(letter)){
+    letter = tolower(letter);
+    cout<<" your letter in lower case:- "<< letter;
+    }
+    return 0;
+}
