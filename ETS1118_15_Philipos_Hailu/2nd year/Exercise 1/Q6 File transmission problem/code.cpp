9caaf700167fe028f1609aad60e3ec91f7bef0c1
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-11-18 10:46:01 +0300

    Update code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q6 File transmission problem/code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year/Exercise 1/Q6 File transmission problem/code.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream>
+
+int main() {
+   using namespace std;
+   int FsizeInBytes, TimeinSec ;//FsizeInBytes = file size in bytes,TimeinSec = time it takes to send a file in seconds
+   cout<<" enter the file size in bytes:- ";
+   cin>> FsizeInBytes;
+   TimeinSec = FsizeInBytes/960;
+   cout<<"the time it takes to send a file in seconds is:- "<< TimeinSec;
+
+    return 0;
+}
