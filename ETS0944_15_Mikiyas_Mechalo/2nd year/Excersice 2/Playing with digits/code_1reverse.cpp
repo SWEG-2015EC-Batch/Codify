e24a4876db5d48d9cff7751c0c0acb8e6ff7eace
Mikiyas Mechalo <77005039+mikiyasmechalo@users.noreply.github.com>
2023-12-06 15:06:12 +0300

    Update code_1reverse.cpp

--- ETS0944_15_Mikiyas_Mechalo/2nd year/Excersice 2/Playing with digits/code_1reverse.cpp
+++ ETS0944_15_Mikiyas_Mechalo/2nd year/Excersice 2/Playing with digits/code_1reverse.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 int main(){
 
-    int num, reversedNum = 0;
+    int num, reversedNum = 0, count = 0, sum=0;
 
     cout << "Enter a number: ";
     cin >> num;
 
     while(num != 0){
+
         reversedNum = reversedNum*10 + num%10;
         num /= 10;
+        count++;
+        sum+=(num%10);
     }
 
-    cout << "The reversed number is " << reversedNum;
+    cout << "The reversed number is " << reversedNum << '\n';
+    cout << "The number of digits is " << count << '\n';
+    cout << "The sum of the digits is " << sum << '\n';
 
 
     return 0;
