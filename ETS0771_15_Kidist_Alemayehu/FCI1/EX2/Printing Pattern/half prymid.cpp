e8e99100287421f1684a83cb1ea2e479da005516
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-12-18 13:17:33 -0800

    Update half prymid.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/half prymid.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/half prymid.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    int hight;
+    cout<< "Enter the hight of the triangle: ";
+    cin >> hight;
+
+    for(int i = 0; i < hight; i++){
+        for(int j = 0; j<=i; j++){
+            cout << " *";
+        }
+        
+        cout << '\n';
+    }
+  return 0;
+}
