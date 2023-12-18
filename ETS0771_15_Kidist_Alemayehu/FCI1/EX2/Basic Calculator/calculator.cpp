ed85928c6609f2c9bfb45b4e810771e3968cd18f
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-12-18 04:59:56 -0800

    Update calculator.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Basic Calculator/calculator.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Basic Calculator/calculator.cpp
@@ -1,1 +1,50 @@
+//this programme will display simple calculator
 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int exit;
+    float x, y;
+    char operatoor;
+
+
+    do {
+        cout << "Enter the num1: ";
+        cin >> x;
+        cout << "Enter an operator +, -, *, /: ";
+        cin >> operatoor;
+        cout << "Enter the num2: ";
+        cin >> y;
+
+        switch (operatoor)
+        {
+        case '+':
+            cout << "The result is " << x + y <<'\n';
+            break;
+        case '-':
+            cout << "The result is " << x - y <<'\n';
+            break;
+        case '*':
+        case 'x':
+        case 'X':
+            cout << "The result is " << x * y <<'\n';
+            break;
+        case '/':
+            cout << "The result is " << x / y <<'\n';
+            break;
+        
+        default:
+            cout << "invalid \n";
+            break;
+        }
+
+        cout << "Enter any key to continue or 0 to exit: ";
+        cin >> exit;
+        if(exit != 0)    cout << '\n';
+
+    }while (exit!=0);
+
+    return 0;
+}
