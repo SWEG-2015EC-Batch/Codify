a36c3b955d229b0f7ce8306c36a6f73db43ef2a9
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-12-18 18:17:42 +0300

    Update number pattern 2.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/number pattern 2.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/number pattern 2.cpp
@@ -26,3 +26,39 @@ int main()
     cout<<endl;
    }
 }
+
+
+
+//this program prints squares using same number sequence in every row incrising by 1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   int row,colom,num,orig;
+
+   cout<<"Enter the number of row: ";
+   cin>>row;
+   cout<< "Enter the number of colom: ";
+   cin>>colom;
+   cout<<"by which number should it start: ";
+   cin>>num;
+
+   orig = num;
+
+   for(int r=1;r<=row;r++) {
+    for(int c=1;c<=colom;c++) {
+        if (c<=r){
+            cout<<num++<<" ";
+        }else {
+            cout<< "  ";
+        }
+        
+    }
+    num = orig;
+    cout<<endl;
+   }
+}
