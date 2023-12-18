1b142e33a4b5ffe4b44f6fe25db064953822fcfe
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:37:38 +0300

    Create pyramid.cpp

---
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/patterns/pyramid.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "Enter the height of the pyramid: ";
+    cin >> height;
+
+    for(int i =0;i<height;i++){
+        for(int j = i; j>0; j--){
+            cout << " ";
+        }
+        for(int j = i; j<height; j++){
+            if(i%2==0){
+                cout << " *";
+            }else{
+                cout << " *";
+            }
+        }
+        if(i!=height-1){
+            cout << endl;
+        }
+    }
+    return 0;
+}
