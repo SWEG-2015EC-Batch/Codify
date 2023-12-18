c37a4cb09843b5ea81a58900ef5ebb5d59b4232c
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:25:22 +0300

    Update digits sum calculator.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/digits sum calculator.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/digits sum calculator.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, sum = 0;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+ int num=number;
+
+    while (number != 0) {
+        int temp = number % 10;
+        sum += temp;
+        number /= 10;
+    }
+
+    cout << "Sum of the digits of " << num<<" is " << sum << endl;
+
+    return 0;
+}
 
