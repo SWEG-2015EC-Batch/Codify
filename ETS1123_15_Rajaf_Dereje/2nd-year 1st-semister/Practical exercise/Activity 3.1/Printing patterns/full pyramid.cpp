aac4d005e6dd73af466399c479edac6f47b3adb6
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-12-18 19:16:14 +0300

    Update full pyramid.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/full pyramid.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/full pyramid.cpp
@@ -1,1 +1,25 @@
+//this program prints a pyramid pattern
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "Enter the height of the pyramid: ";
+    cin >> height;
+
+    for (int i = 1; i <= height; ++i) {
+        for (int j = 1; j <= height - i; ++j){ // Print spaces for the left side of the pyramid
+            cout << "  ";
+        }   
+        
+        for (int k = 1; k <= 2 * i - 1; ++k){ // Print '*' for the left and right sides of the pyramid
+            cout << " *";
+        }
+
+        cout << '\n';   // Move to the next line after each row
+    }
+    return 0;
+}
 
