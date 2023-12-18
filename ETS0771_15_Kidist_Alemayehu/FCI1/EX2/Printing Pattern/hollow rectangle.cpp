6dd4443eda9a13dd7993d69cf3898acd30f41d60
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-12-18 13:14:35 -0800

    Update hollow rectangle.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/hollow rectangle.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/hollow rectangle.cpp
@@ -1,1 +1,26 @@
 
+// this will display patten of hollow triangle
+
+#include<iostream>
+using namespace std;
+int width, length;
+    cout << "Enter the rectangle width: ";
+    cin >> length;
+    cout << "Enter the rectangle length: ";
+    cin >> width;
+
+    for(int i = 1; i <= width; i++){
+    
+        for(int j = 1; j <= length; j++){
+
+            if(i == 1 || i == width || j == 1 || j == length){
+                cout << " *";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << "\n";
+    }
+return 0;
+}
