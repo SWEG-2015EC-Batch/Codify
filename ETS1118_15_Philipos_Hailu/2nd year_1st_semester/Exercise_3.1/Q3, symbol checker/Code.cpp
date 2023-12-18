5b52f863873c70c77762b8a50e5bfcfe1f1186da
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-12-18 21:36:30 +0300

    Update Code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q3, symbol checker/Code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q3, symbol checker/Code.cpp
@@ -1,1 +1,40 @@
+// This program tells whether a character entered from the keyboard is in upper case or in lower case , a vowel or a consonant or digit or special character
 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    char character;
+    
+    cout << "Enter any character: ";
+    cin >> character;
+    
+    if (isalpha(character)){
+        cout << "The character you entered is an alphabet" << endl;
+        if (isupper(character)){
+            cout << "an upper case and" << endl;
+        }
+        else{
+            cout << "a lower case and" << endl;
+        }
+        if(character=='a' || character=='e' || character=='i' || character=='o' || character=='u' || character=='A' || character=='E' || character=='I' || character=='O' || character=='U'){
+            cout << "a vowel" << endl;
+        }
+        else{
+            cout << "a consonant" << endl;
+        }
+    }
+    
+    
+    else if(isdigit(character)){
+        cout << "The character you entered is a digit" << endl;
+    }
+    else{
+        cout << "The character you entered is a special character" << endl;
+    }
+    
+    
+    return 0;
+}
