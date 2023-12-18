828df8fd2f28d9f13098d51e893fc3d0301dc4c4
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-12-18 19:10:24 +0300

    Update number patterns.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/number patterns.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/number patterns.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row,colom,num;
+    char character;
+
+    //to print numbers from 10-49 when its 4x5
+    
+    cout<< "Enter the number you want to start with: ";
+    cin>> num;
+    cout << "Enter the number of rows: ";
+    cin>>row;
+    cout<< "Enter the number of colom: ";
+    cin>>colom;
+
+    for (int r=1;r<=row;r++) {
+        for (int c=1;c<=colom;c++) {
+            cout<< num<< " ";
+            num ++;
+        }
+        cout<<endl;
+    }
+
+ 
+
+ 
+
+   
+
+    return 0;
+}
 
