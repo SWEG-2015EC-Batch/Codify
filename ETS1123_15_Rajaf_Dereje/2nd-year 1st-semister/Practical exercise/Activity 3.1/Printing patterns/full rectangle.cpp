73995373a7e4198691d6dc0eac5e2b566b3a80d8
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-12-18 19:01:30 +0300

    Update full rectangle.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/full rectangle.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/full rectangle.cpp
@@ -1,1 +1,28 @@
+ //for a full square
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int row,colom;
+    char character;
+ 
+    cout<< "Enter the character you want to build you squares with: ";
+    cin>> character;
+    cout << "Enter the number of rows: ";
+    cin>>row;
+    cout<< "Enter the number of colom: ";
+    cin>>colom;
+
+    for (int r=1; r<=row;r++) {
+        for (int c=1; c<=colom;c++) {
+            cout<< character<< " ";
+        }
+        cout<<endl;
+    }
+    */
+
+
+    return 0;
+}
