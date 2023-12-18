6ac345cf3c24e939277efde788de7bf5a6660cda
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:27:05 +0300

    Update Even digit multiplier.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/Even digit multiplier.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/Even digit multiplier.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, originalNumber, evenProduct = 1;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    originalNumber=number;
+    while (number > 0) {
+        int temp = number % 10;
+        if (temp % 2 == 0) {
+            evenProduct *= temp;
+        }
+        number /= 10;
+    }
+    cout<< "product of even numbers in the number "<<originalNumber<<" is " <<evenProduct<<endl;
+
+    return 0;
+}
 
