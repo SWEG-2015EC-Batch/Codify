85d614ce156276f201348edd9e87c4755add9cf1
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:15:52 +0300

    Update case.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Case checker/case.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Case checker/case.cpp
@@ -1,1 +1,56 @@
+//This program tells whether a character entered from a user is special character, an alphabet or a number
+#include <iostream>
+#include <cctype>
+using namespace std;
 
+int main() {
+    
+    char key;
+// read the input 
+   
+    cout << "press a character on the keyboard: ";
+    cin >> key;
+ 
+ // check if the input is an alphabet
+ 
+  if(isalpha(key)) 
+  {
+   
+     char upperKey = toupper(key);
+     if (upperKey =='A' || upperKey =='E' || upperKey=='I' || upperKey=='O' || upperKey=='U'){
+          
+           if(isupper(key)){
+           cout<< "The key you entered is a vowel, uppercase letter." <<endl;}
+           else {
+            cout<< "The key you entered is a vowel, lowercase letter." <<endl;}
+   
+           }
+     else
+  {
+
+        if(isupper(key)){
+        cout<< "The key you entered is a consonant, uppercase letter." <<endl ;}
+        else {
+        cout<< "The key you entered is a consonant, lowercase letter." <<endl;}
+  }
+ }
+       
+       
+       //check if the input is a number
+       
+ else if(isdigit(key))
+    {
+            if(key%2 == 0){
+                cout<<"The key you entered is an even number." <<endl;}
+             else{ 
+                cout<<"The key you entered is an odd number." <<endl;}
+    }
+      
+       //checking if the input is neither alphabet nor number to decide if it is a special character
+        
+        else if (!isalnum(key)){
+            cout<< "the key you entered is a special character." <<endl ;
+   }
+
+return 0;
+}
