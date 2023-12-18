b5b1a393b8c255305422ef2309a720cf6e86f300
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:28:10 +0300

    Update frequency.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/frequency.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/frequency.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int frequency[10] = {0};
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    // Count the frequency of each digit
+    while (num != 0) {
+        int digit = num % 10;
+        frequency[digit]++;
+        num = num / 10;
+    }
+
+    // Print the frequency table
+    cout << "Digit   Frequency" << endl;
+    for (int i = 0; i < 10; i++) {
+        if (frequency[i] > 0) {
+            cout << "  " << i << "        " << frequency[i] << endl;
+        }
+    }
+
+    return 0;
+}
 
