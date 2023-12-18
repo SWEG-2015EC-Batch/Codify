d7bc9e10c6d9a0ebc098f93cab13b210691b23a4
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-12-18 13:23:32 -0800

    Update triangle.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/triangle.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/triangle.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    int hight;
+    cout<< "Enter the hight of the triangle: ";
+    cin >> hight;
+
+    int temp = hight;
+    for(int i = hight; i >= 1; i--){
+        for(int j = 1; j<=i; j++){
+            if(j == 1 || i == hight || j == i){
+                cout << " *";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << '\n';
+    }
+
+  return 0;
+}
