cdaf8f4af3e89b317b9da89a520f29ef5ab19ffa
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:21:57 +0300

    Update Armstrong.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/Armstrong.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/Armstrong.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int number, originalNumber, temp, result = 0;
+
+    cout << "Enter a 3-digit integer: ";
+    cin >> number;
+
+    originalNumber = number;
+
+    while (number != 0) {
+        temp = number % 10;
+        result += (pow(temp,3));
+       number /= 10;
+    }
+
+    if (result == originalNumber) {
+        cout << "The number is an Armstrong number." << endl;
+    } else {
+        cout << "The number is not an Armstrong number." << endl;
+    }
+
+    return 0;
+}
 
