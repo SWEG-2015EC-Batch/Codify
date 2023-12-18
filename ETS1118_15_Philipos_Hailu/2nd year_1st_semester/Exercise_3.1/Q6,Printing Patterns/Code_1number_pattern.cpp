80f96fa4a37b77b9866641d988290860b37bf3b5
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-12-18 21:49:37 +0300

    Update Code_1number_pattern.cpp

--- ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q6,Printing Patterns/Code_1number_pattern.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q6,Printing Patterns/Code_1number_pattern.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+int main(){
+
+	int value = 10;
+	for (int i = 0; i != 4; ++i)
+	{
+		for (int j = 0; j != 10; ++j)
+		{
+	    cout << value++ << " ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
