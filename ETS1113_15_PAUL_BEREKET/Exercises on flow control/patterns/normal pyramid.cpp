1f251d1f6395b415729c81d1e395a8ec3f1e3a1e
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:40:22 +0300

    Create normal pyramid.cpp

---
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/patterns/normal pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "Enter the height of the pyramid: ";
+    cin >> height;
+
+    for (int i = 1; i <= height; ++i) {
+        for (int j = 1; j <= height - i; ++j){
+            cout << "  ";
+        }
+
+        for (int k = 1; k <= 2 * i - 1; ++k){
+            cout << " *";
+        }
+
+        cout << '\n';
+    }
+    return 0;
+}
