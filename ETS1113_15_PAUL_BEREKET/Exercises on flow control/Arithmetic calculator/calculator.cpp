5c0a14f7221f511e54c3b73b1674f0fee2a982c3
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:14:26 +0300

    Update calculator.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Arithmetic calculator/calculator.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Arithmetic calculator/calculator.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    char operatorr;
+    int exi;
+    double num1, num2, result;
+do{
+
+    cout << "Enter an operatorr (+, -, *, /): ";
+    cin >> operatorr;
+
+    cout << "Enter the first number: ";
+    cin >> num1 ;
+    cout<<"Enter the second number: " ;
+    cin>>num2;
+
+    switch (operatorr) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result<<endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result<<endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result<<endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result<<endl;
+            } else {
+                cout << "Error: Division by zero!"<<endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator!"<<endl;
+
+}
+    cout<<"Enetr zero to exit or enter other number to continue: ";
+    cin>> exi;
+    }while(exi!=0);
+
+    return 0;
+}
