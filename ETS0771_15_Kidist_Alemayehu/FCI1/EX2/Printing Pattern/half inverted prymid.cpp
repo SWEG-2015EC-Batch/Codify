c2622af55f2c0ec5d5ac57bf59f3a30cdd8b231b
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-12-18 13:18:50 -0800

    Update half inverted prymid.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/half inverted prymid.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX2/Printing Pattern/half inverted prymid.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    int hight;
+    cout<< "Enter the hight of the triangle: ";
+    cin >> hight
+    for(int i = 0; i < hight; i++){
+        for(int j = i; j<hight; j++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+  return 0;
+
+}
+
+    
