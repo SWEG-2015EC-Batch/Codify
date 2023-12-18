d1e16ba109a79d0bc5f92393c65eec0d749e515b
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2023-12-18 21:31:41 +0300

    Update Code.cpp

--- ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q2,BasicCalculatorApplication/Code.cpp
+++ ETS1118_15_Philipos_Hailu/2nd year_1st_semester/Exercise_3.1/Q2,BasicCalculatorApplication/Code.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    float num1, num2, result;
+    char op;// op = operator
+    int n;
+    do{
+   
+    cout<< " enter num1: ";
+    cin>>num1;
+    cout<< " enter num2: ";
+    cin>>num2;
+    cout<< " enter operator: ";
+    cin>>op;
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;    
+        case '*':
+            result = num1 * num2;  
+            break;  
+        case '/':
+            result = num1 / num2;    
+            break;
+        default:
+            cout<< "error due to unknown operator";    
+        }    
+        cout<<result <<endl; 
+        cout<< "Enter 0 to terminate the program or other integers to continue: ";
+        cin>> n;  
+        } while (n != 0);
+    return 0;
+}
