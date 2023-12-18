4c77576238d32eb38c9217c12bd4a91dbc261fef
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2023-12-18 19:03:35 +0300

    Update hollow rectangle.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/hollow rectangle.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 3.1/Printing patterns/hollow rectangle.cpp
@@ -1,1 +1,45 @@
+//this program prints the patter of a hallow square
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row,colom;
+    char character;
+
+    
+    
+    
+    cout<< "Enter the character you want to build you squares with: ";
+    cin>> character;
+    cout << "Enter the number of rows: ";
+    cin>>row;
+    cout<< "Enter the number of colom: ";
+    cin>>colom;
+
+    for (int r=1;r<=row;r++) {
+        for (int c=1;c<=colom;c++) {
+            if (r==1||r==row||c==1||c==colom) {
+                cout<< character<< " ";
+            }
+            else {
+                cout<< "  ";
+            }
+        }
+        cout<<endl;
+    }
+    
+
+   
+
+ 
+
+  
+
+   
+
+
+    return 0;
+}
 
