32ad420c57379741958d9bef2aa0b0e8a9ccaa2e
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:19:58 +0300

    Update reverse.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/reverse.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/reverse.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, revers = 0;
+
+    cout << "Enter any ineger: ";
+    cin >> num;
+
+    while (num != 0) {
+        int temp = num % 10;
+        revers = revers * 10 + temp;
+        num /= 10;
+    }
+
+    cout << "Reversed number: " << revers << endl;
+
+    return 0;
+}
 
