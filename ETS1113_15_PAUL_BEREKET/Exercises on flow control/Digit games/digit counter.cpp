4376ac10de41a4113b4301adc04d51a67cec4a42
Paul Bereket <148947356+PolA1118@users.noreply.github.com>
2023-12-18 23:24:37 +0300

    Update digit counter.cpp

--- ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/digit counter.cpp
+++ ETS1113_15_PAUL_BEREKET/Exercises on flow control/Digit games/digit counter.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int num,counter=0 ;
+ cout<<"Enter an integer: ";
+ cin>>num;
+ while(num!=0){
+    num/=10;
+    counter++;
+
+ }
+ cout<<"The number of digit(s) is/are "<<counter;
+
+    return 0;
+}
 
