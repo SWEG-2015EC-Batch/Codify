65fbbe10d9a7d66681a24f7c77c07af3111bdfba
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-01-05 13:49:08 +0300

    Update 1.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/1.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/1.cpp
@@ -1,1 +1,12 @@
+/*
+This program initialazes a 4 by 5 array called values and intialize them
+*/
+#include <iostream>
+using namespace std;
 
+int main() {
+    int values[4][5]={16, 22, 99, 4, 18, -258, 4,
+101, 5, 98, 105, 6, 15, 2, 45, 33, 88, 72, 16, 3};
+
+return 0;
+}
