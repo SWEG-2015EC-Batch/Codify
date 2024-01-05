fb163d451dc88eb756b412be894856df478a4a9b
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-01-05 14:58:46 +0300

    Update 7.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/7.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/7.cpp
@@ -1,1 +1,27 @@
+/*
+This program stores a list of number and print the even numbers in the array in reverse order they
+are entered.
+*/
+#include <iostream>
+using namespace std;
 
+int main() {
+    int N;// stands for number of values
+    
+    cout<<"How many numbers would you like to enter ";
+    cin>>N;
+    int nums[N];
+    for(int i=0; i<N;i++){
+        cout<<"Num"<<i+1<<": ";
+        cin>>nums[i];
+    }
+    
+    cout<<"\nFrom the values you've enterd the even numbers are\n";
+    for(int i=N; i>0;i--){
+        if(nums[i-1]%2==0){
+            cout<<nums[i-1]<<endl;
+        }
+    }
+
+return 0;
+}
