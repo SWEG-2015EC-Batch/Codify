abfe3a3dde86b47579eb8339c8583d738682ba1c
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-01-05 14:19:22 +0300

    Update 5.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/5.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 4.2/1D array exercises/5.cpp
@@ -1,1 +1,20 @@
+/*
+This program to input the following values in an array named volts: 11.95, 16.32, 12.15, 8.22, 15.98,
+26.22, 13.54, 6.45, and 17.59. After the data has been entered, have your program display it three column
+in table form:
+*/
+#include <iostream>
+using namespace std;
 
+int main() {
+float volts[]={11.95, 16.32, 12.15, 8.22, 15.98, 26.22, 13.54, 6.45, 17.59};
+
+for(int i=0; i<9; i++){
+
+    if(i==3|| i==6){
+        cout<<endl;
+    }
+    cout<<volts[i]<<"\t|\t";
+}
+return 0;
+}
