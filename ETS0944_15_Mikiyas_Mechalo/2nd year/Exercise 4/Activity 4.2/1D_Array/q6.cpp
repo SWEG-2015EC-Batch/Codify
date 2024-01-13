616a74f35b0b0486f3cde66b12bd064dd354b5aa
Mikiyas Mechalo <77005039+mikiyasmechalo@users.noreply.github.com>
2024-01-13 18:14:15 +0300

    Create q6.cpp

---
+++ ETS0944_15_Mikiyas_Mechalo/2nd year/Exercise 4/Activity 4.2/1D_Array/q6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    for (int i = 16; i >= 0; i--)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            cout << numbers[i] << ", ";
+        }
+        
+    }
+  
+    return 0;
+    
+}
