ef462190375ab08a6fe2eb0d38038105e3377aac
Mikiyas Mechalo <77005039+mikiyasmechalo@users.noreply.github.com>
2024-01-13 18:23:20 +0300

    Create q1.cpp

---
+++ ETS0944_15_Mikiyas_Mechalo/2nd year/Exercise 4/Activity 4.2/String/q1.cpp
@@ -0,0 +1,42 @@
+//Write a program that read a string and count number of vowels and consonants alphabet present in a string.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    string word;
+    cout << "Enter a word or a sentence: ";
+    getline(cin, word);
+    
+    int vowel= 0, consonant = 0;
+
+    for (int i=0; i<word.length(); i++){
+        char temp = toupper(word[i]);
+        switch (temp){
+        case 'A':
+            vowel +=1;
+            break;
+        case 'E':
+            vowel +=1;
+            break;
+        case 'I':
+            vowel +=1;
+            break;
+        case 'O':
+            vowel +=1;
+            break;
+        case 'U':
+            vowel +=1;
+            break;
+        default:
+            consonant +=1;
+            break;
+        }
+    }
+
+    cout << "We have " << vowel << " vowel letters";
+    cout << " and " << consonant << " consonant letters.\n";
+    
+    return 0;
+}
