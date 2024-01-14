0df5e16e2aac57f48ab6d563aeb6d8e55c56f27d
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 13:46:50 -0800

    Update question6.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question6.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question6.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int numStudents;
+    cout << "Enter the number of students: ";
+    cin >> numStudents;
+
+    int marks[numStudents];
+    string levels[numStudents];
+
+    for (int i = 0; i < numStudents; i++) {
+        cout << "Enter total marks for student " << i+1 << ": ";
+        cin >> marks[i];
+
+        if (marks[i] >= 80) {
+            levels[i] = "Excellent";
+        } else if (marks[i] >= 60) {
+            levels[i] = "Very Good";
+        } else if (marks[i] >= 50) {
+            levels[i] = "Fair";
+        } else if (marks[i] >= 40) {
+            levels[i] = "Poor";
+        } else {
+            levels[i] = "Fail";
+        }
+    }
+
+    cout << "\nScore\tLevel" << endl;
+    for (int i = 0; i < numStudents; i++) {
+        cout << marks[i] << "\t" << levels[i] << endl;
+    }
+
+    return 0;
+}
