6571631eef1594a8a79ba2866937114f3a62d30f
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 13:10:51 -0800

    Update question1.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/2D Array/question1.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/2D Array/question1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+int main() {
+    const int SIZE = 5;
+    int arr1[SIZE], arr2[SIZE];
+
+    cout << "Enter elements of first array: ";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter elements of second array: ";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> arr2[i];
+    }
+
+    bool identical = true;
+    for (int i = 0; i < SIZE; i++) {
+        if (arr1[i] != arr2[i]) {
+            identical = false;
+            break;
+        }
+    }
+
+    if (identical) {
+        cout << "The arrays are identical." << endl;
+    } else {
+        cout << "The arrays are not identical." << endl;
+    }
+
+    return 0;
+}
