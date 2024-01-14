1f6040d5102ef8367c8960fad9fc7b12fbdf98ec
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 12:43:08 -0800

    Update question1.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question1.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main() {
+    int arr[4][5] = {{16, 22, 99, 4, 18}, {-258, 4, 101, 5, 98}, {105, 6, 15, 2, 45}, {33, 88, 72, 16, 3}};
+
+    // print the array
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
