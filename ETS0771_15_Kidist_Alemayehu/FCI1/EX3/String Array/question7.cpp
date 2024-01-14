040a7fe59449279bc86311ccb0b54747c4cf790a
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 14:00:40 -0800

    Update question7.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question7.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question7.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+
+
+int main() {
+
+    int size = 5;
+    string names[size];
+
+    for (int i = 0; i < size; i++){
+       std:: cout << "Enter name #" << i+1 << ": ";
+        getline(cin, names[i]);
+    }
+    
+    for(int i = 0; i < size -1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            string temp;
+            if(names[j][0] > names[j +1][0]){
+                temp = names[j];
+                names[j] = names[j+1];
+                names[j+1] = temp;
+            }
+        }
+    }
+
+    std::cout << "The names in alphabetical order:-\n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << names[i] << '\n';
+    }
+
+    return 0;
+}
