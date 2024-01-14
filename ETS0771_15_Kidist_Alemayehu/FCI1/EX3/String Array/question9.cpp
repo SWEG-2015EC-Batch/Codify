0d7886b55a65c728ae07f0f14a1038510f9849e2
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 14:12:02 -0800

    Update question9.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question9.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question9.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+int main() {
+    char password[] = {"MyPassword123"}; 
+    string passwordTry;
+
+    cout << "Enter the password: ";
+    getline(cin, passwordTry);
+
+    for(int i=0; i<strlen(password);i++){
+        if(passwordTry[i] != password[i]){
+            cout << "Incorrect Password!";
+            return 0;
+        }
+    }
+    cout << "Correct!";
+
+    return 0;
+}
