7230ef880edd1c6e90d95a6a374a1205bcb3dae3
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2024-01-14 17:48:10 +0300

    Create Q9.cpp

---
+++ ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main(){
+
+    char firstProgram[] = "over lord";
+    char secondProgram[150];
+    strcpy(secondProgram,"The life of the holy");
+
+    cout<<"Your favorite Tv programs are: "<<firstProgram<<", "<<secondProgram<<endl;
+
+    return 0;
+}
