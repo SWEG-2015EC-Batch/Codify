a9810919ac77cba326c1157693bddc24400b59b1
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2024-01-14 17:47:47 +0300

    Create Q8.cpp

---
+++ ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q8.cpp
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main(){
+
+    string input;
+    char ch;
+
+    cout<<"Enter a line of text: ";
+    getline(cin,input);
+
+    cout<<"The uppercase vesrsion of "<<"\""<<input<<"\""<<" is: ";
+
+    for (int i = 0; i < input.length(); i++){
+
+        ch = toupper(input[i]);
+        cout<<ch;
+        
+    }
+    cout<<endl;
+
+    cout<<"The lowercase vesrsion of "<<"\""<<input<<"\""<<" is: ";
+
+    for (int i = 0; i < input.length(); i++){
+
+        ch = tolower(input[i]);
+        cout<<ch;
+        
+    }
+    cout<<endl;
+
+    return 0;
+
+}
