30349940ec6b438291bb0778145f10c37550b62a
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 13:48:43 -0800

    Update question4.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question4.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question4.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstring>
 
+int main() {
+    const int MAX_LENGTH = 100;
+    char input[MAX_LENGTH];
+    char tokens[MAX_LENGTH][MAX_LENGTH];
+    int token_count = 0;
+
+    std::cout << "Enter a line of text: ";
+    std::cin.getline(input, MAX_LENGTH);
+
+    char* token = std::strtok(input, " ");
+    while (token != nullptr) {
+        std::strcpy(tokens[token_count], token);
+        token_count++;
+        token = std::strtok(nullptr, " ");
+    }
+
+    std::cout << "The tokens are:\n";
+    for (int i = 0; i < token_count; i++) {
+        std::cout << tokens[i] << std::endl;
+    }
+
+    return 0;
+}
