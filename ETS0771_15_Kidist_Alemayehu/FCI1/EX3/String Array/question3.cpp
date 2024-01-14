1934070add91c91cdc556f78890c85be3ffd1315
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 13:39:48 -0800

    Update question3.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question3.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstring>
 
+int main() {
+    const int MAX_LENGTH = 100;
+    char input[MAX_LENGTH];
+    char output[MAX_LENGTH];
+    int j = 0;
+
+    std::cout << "Enter a string: ";
+    std::cin.getline(input, MAX_LENGTH);
+
+    int length = std::strlen(input);
+
+    for (int i = 0; i < length; i++) {
+        if (std::isalpha(input[i])) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    output[j] = '\0';
+
+    std::cout << "The alphabetic characters in the string are: " << output << std::endl;
+
+    return 0;
+}
