418644c02c6540e45941731fec9335dde2923a50
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 13:03:31 -0800

    Update question7.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question7.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question7.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> nums;
+    int num;
 
+    cout << "Enter a list of numbers (0 to stop): ";
+
+    // Read numbers until 0 is entered
+    while (true) {
+        cin >> num;
+        if (num == 0) {
+            break;
+        }
+        nums.push_back(num);
+    }
+
+    cout << "Even numbers in reverse order: ";
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        if (nums[i] % 2 == 0) {
+            << nums[i] << " ";
+        }
+    }
+    cout <<endl;
+
+    return 0;
+}
