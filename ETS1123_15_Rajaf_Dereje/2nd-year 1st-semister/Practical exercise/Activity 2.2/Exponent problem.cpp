a38226edc540bc18009f7092093733446a9714cc
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-01-14 12:01:10 +0300

    Create Exponent problem.cpp

---
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Practical exercise/Activity 2.2/Exponent problem.cpp
@@ -0,0 +1,17 @@
+//this program finds takes base number and exponet number and fides their power
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ double x,y;
+ cout<<"enter the base number :";
+ cin>>x;
+ cout<<"enter the exponent :";
+ cin>>y;
+ pow(x,y) ;
+ cout<<"the resulting value is :"<<pow(x,y);
+    return 0;
+}
