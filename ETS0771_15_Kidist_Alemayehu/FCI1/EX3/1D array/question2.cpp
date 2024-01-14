16b7750019a6a501ca27a849172e3469f1c31f2e
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 12:46:32 -0800

    Update question2.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question2.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/1D array/question2.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int temp[8], total = 0;
+
+    // input numbers and calculate total
+    for (int i = 0; i < 8; i++) {
+        cout << "Enter number " << i+1 << ": ";
+        cin >> temp[i];
+        total += temp[i];
+    }
+
+    // calculate average
+    double average = static_cast<double>(total) / 8;
+
+    // display total and average
+    cout << "Total: " << total << std::endl;
+    cout << "Average: " << average << std::endl;
+
+    return 0;
+}
+
 
