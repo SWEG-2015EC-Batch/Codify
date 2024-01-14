a4b502077ac98fa00a3e19931e01d46bc4d41743
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2024-01-14 17:45:11 +0300

    Create Q3.cpp

---
+++ ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    string mix,temp = "";
+
+    cout<<"Enter a string: ";
+    getline(cin,mix);
+
+    int sizeOfmix = mix.size();
+
+    for (int i = 0; i < sizeOfmix; i++){
+
+        if((mix[i] >= 'a' && mix[i] <= 'z') || (mix[i] >= 'A' && mix[i] <= 'Z' ) ){
+
+            temp += mix[i];
+        }
+
+    }
+
+    mix = temp;
+
+    cout<<"output of only alphanumerical characters of the string: "<<mix<<endl;
+  
+    return 0;
+
+}
