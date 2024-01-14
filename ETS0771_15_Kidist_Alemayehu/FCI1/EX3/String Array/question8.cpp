e9ff5a1613f8ff2205600988e88d1da9a595a1f5
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 14:13:30 -0800

    Update question8.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question8.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question8.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+
+    string word;
+
+    cout << "Enter a line of text: ";
+    getline(cin, word);
+
+    string upper = word, lower = word;
+
+    for (char& ch : upper) {
+        ch = toupper(ch);
+    }
+    
+    for (char& ch : lower) {
+        ch = tolower(ch);
+    }
+    
+    cout << "All in upper case: " << upper << '\n';
+    cout << "All in lower case: " << lower << '\n';
+
+
+
+    return 0;
+}
