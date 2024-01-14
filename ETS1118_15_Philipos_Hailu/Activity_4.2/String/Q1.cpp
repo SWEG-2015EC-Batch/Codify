6ff96de2ae7f3eeaaf900591eeeddbc1960d4fb1
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2024-01-14 17:43:59 +0300

    Update Q1.cpp

--- ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q1.cpp
+++ ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
 
+    char paragraph[150];
+    int vowles = 0, consonants = 0;
+
+    cout<<"Enter a string: ";
+    cin.getline (paragraph,150);
+
+    for (int i = 0; paragraph[i] != '\0' ; i++){
+
+        if(paragraph[i] == 'a' || paragraph[i] == 'e' || paragraph[i] == 'i' || paragraph[i] == 'o' || paragraph[i] == 'u' 
+        || paragraph[i] == 'A' || paragraph[i] == 'E' || paragraph[i] == 'I' || paragraph[i] == 'O' || paragraph[i] == 'U'){
+            vowles++;
+        }else if((paragraph[i] >= 'a' && paragraph[i] <= 'z') || (paragraph[i] >= 'A' && paragraph[i] <= 'Z' ) ){
+            ++consonants;
+        }
+        
+    }
+
+    cout<< "The number of vowles: "<<vowles<<endl;
+    cout<< "The number of consonants: "<<consonants<<endl;
+    
+    return 0;
+}
