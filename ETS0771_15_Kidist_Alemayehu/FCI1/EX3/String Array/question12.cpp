34fba91c389eac1575f1145d4bbbe258b1a74165
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 14:04:17 -0800

    Update question12.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question12.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question12.cpp
@@ -1,1 +1,41 @@
 
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+int main() {
+
+    char answers[]={'A', 'B','C', 'D', 'D', 'C', 'B', 'D', 'A', 'B'};
+
+    char answerTry[10];
+    int score =0;
+
+    for (int i=0; i<10; i++){
+        cout << "Enter answer for Q #" << i+1 << ": ";
+        char temp;
+        cin >> temp;
+        answerTry[i]= char(toupper(temp));
+        if(answerTry[i]==answers[i]){
+            score++;
+        }
+    }
+
+    cout << "\nYour answers are: ";
+
+    for(int i =0; i <10; i++){
+        cout << answerTry[i];
+        if (i!=9) cout << ", ";
+    }
+    cout << "\nThe correct answers are: ";
+
+    for(int i =0; i <10; i++){
+        cout << answers[i];
+        if (i!=9) cout << ", ";
+    }
+
+    cout << "\nYou scored " << score << " out of 10";
+
+
+
+    return 0;
+}
