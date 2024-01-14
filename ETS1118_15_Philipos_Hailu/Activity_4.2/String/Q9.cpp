76f02782821a303fb1a979a025cdc92c506d6b7b
@Philipos <148946615+PhiliposHailu@users.noreply.github.com>
2024-01-14 21:33:58 +0300

    Update Q9.cpp

--- ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q9.cpp
+++ ETS1118_15_Philipos_Hailu/Activity_4.2/String/Q9.cpp
@@ -1,3 +1,4 @@
+// a program to store and print the names of your two favorite television programs.Initialize one of the strings (assign it the first program’s name) at the time you declare the array. Initialize the second value in the body of the program with the strcpy() function.
 #include <iostream>
 #include <string.h>
 
