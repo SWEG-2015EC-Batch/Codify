f554b97740199bca9cdcb8b6ea92721e3befc2a1
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-14 14:05:57 -0800

    Update question11.cpp

--- ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question11.cpp
+++ ETS0771_15_Kidist_Alemayehu/FCI1/EX3/String Array/question11.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+int main() {
+
+    string name[3];
+
+    cout << "Enter your first name: ";
+    cin >> name[0];
+    cout << "Enter your middle name: ";
+    cin >> name[1];
+    cout << "Enter your last name: ";
+    cin >> name[2];
+
+    cout << "Your initias are " << name[0].front() << name[1].front() << name[2].front() << ' ';
+    cout << "Or " << char(toupper(name[0].front())) << char(toupper(name[1].front())) << char(toupper(name[2].front()));
+
+    return 0;
+}
