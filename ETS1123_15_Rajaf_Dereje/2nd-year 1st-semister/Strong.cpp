1f8d346a7c17d455f3ec0aa5c4dc06fdca998f75
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-02-28 11:59:10 +0300

    Update Strong.cpp

--- ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Strong.cpp
+++ ETS1123_15_Rajaf_Dereje/2nd-year 1st-semister/Strong.cpp
@@ -1,39 +1,44 @@
-/*
-This program tells if a number is a Strong number or not
-*/
+// This program tells weather a number is strong or not
+// Feb 28, 2024
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-int main() {
+int main(){
+    int num(0), sum(0);
 
-    int num(0),mod,fac(1);
-    cout<< "Enter a number: ";
-    cin>>num;
+    cout<<"\nEnter a negative number to exit the program\n";
+    do{
+        cout<<"Enter number: ";
+        cin>>num;
+        int org(num); //to hold the orignal value if the number for comparision
 
-    int sum(0);
-    while(num>=0){
-        mod = num%10; //to split the number into individual digit
-          if(mod>0){
-            while(mod >0){
-                fac*=mod;
-                --mod;
+        int mod, fact(1);
+        if(num==0){
+            sum = fact;
+        }
+
+        while(num > 0){
+            mod = num%10;  //to split the numbers from the last digit
+            while (mod>=1){
+                fact *= mod;
+                mod--;
             }
-          }
-        sum += fac;
-        num /=10;
-    }
+            sum+=fact;
+            fact=1;
+            num/=10; 
+        }
 
-    switch(num == sum){
-    case 1:
-        cout<< "Your number is strong";
-        break;
-    default :
-        cout<< "Your number is not strong";
-        break;
-    }
-
+        switch (org==sum){
+            case 0:
+                cout<< "Your number is not strong\n";
+                break;
+            default :
+                cout<< "your number is strong\n";
+                break;
+        }
+    }while(num >=0);
 
     return 0;
 }
